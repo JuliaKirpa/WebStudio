1dd589ae924e93b98c1730920d0a7af48dd3f8bc
Gipernova <lia_shongmao@mail.ru>
2022-03-09 13:17:52 +0300

    add time to structs

--- internal/models/Client.go
+++ internal/models/Client.go
@@ -1,11 +1,15 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Client struct {
 	gorm.Model
-	ID        uint   `gorm:"primary_key; auto-increment" json:"id"`
-	Name      string `gorm:"size 255; not-null" json:"name"`
-	Telephone int    `gorm:"varchar(50); not-null" json:"number"`
-	Email     string `gorm:"size 255" json:"email"`
+	ID        uint      `gorm:"primary_key; auto-increment" json:"id"`
+	Name      string    `gorm:"size 255; not-null" json:"name"`
+	Telephone int       `gorm:"varchar(50); not-null" json:"number"`
+	Email     string    `gorm:"size 255" json:"email"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 }
--- internal/models/Order.go
+++ internal/models/Order.go
@@ -1,12 +1,16 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Order struct {
 	gorm.Model
-	ID       uint    `gorm:"primary_key; auto-increment" json:"id"`
-	ClientID uint    `gorm:"not null" json:"client_id"`
-	Service  Service `json:"service"`
-	Done     bool    `gorm:"default:false" json:"done"`
-	Discount uint    `json:"discount"`
+	ID        uint      `gorm:"primary_key; auto-increment" json:"id"`
+	ClientID  uint      `gorm:"not null" json:"client_id"`
+	Service   []Service `json:"service"`
+	Done      bool      `gorm:"default:false" json:"done"`
+	Discount  uint      `json:"discount"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 }
