442878bf0a38d795ecc3a99d76c26463f224f444
Gipernova <lia_shongmao@mail.ru>
2022-03-18 23:16:12 +0300

    add http router

---
+++ pkg/handler/handler.go
@@ -0,0 +1,34 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-in")
+	}
+	order := router.Group("/order")
+	{
+		client := router.Group("/client")
+		{
+			client.POST("/")
+			client.GET("/")
+			client.GET("/:id")
+			client.PUT("/:id")
+			client.DELETE("/:id")
+		}
+
+		servise := router.Group("/service")
+		{
+			servise.POST("/")
+			servise.GET("/")
+			servise.DELETE("/:id")
+		}
+	}
+	return router
+}
