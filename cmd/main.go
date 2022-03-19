ea6ecccadff66be1b677eb22686c68c7934ef6bd
Gipernova <lia_shongmao@mail.ru>
2022-03-19 12:14:56 +0300

    add config file

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,28 @@ import (
 	"ClientBaseWEBStudio/pkg/handler"
 	"ClientBaseWEBStudio/pkg/repository"
 	"ClientBaseWEBStudio/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Fatalf("error from init config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	server := new(api.Server)
-	err := server.Run("8080", handlers.InitRoutes())
-	if err != nil {
-		log.Fatalf("error to running server %s", err)
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error to running server %s", err.Error())
 	}
 
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
